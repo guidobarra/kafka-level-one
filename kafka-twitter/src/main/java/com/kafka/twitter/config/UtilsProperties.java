45159d3e7e8a3c0442468f0a8824b65453cec4f3
Guido_Barra <Guido_Barra@ITRP0478>
2021-02-18 00:21:22 -0300

    read twitters of bitcoin

---
+++ kafka-twitter/src/main/java/com/kafka/twitter/config/UtilsProperties.java
@@ -0,0 +1,12 @@
+package com.kafka.twitter.config;
+
+public class UtilsProperties {
+
+    public static final String CONSUMER_KEY = "XU1OXOUT6uBqEaYVvyntGV4a3";
+
+    public static final String CONSUMER_SECRET = "GhDMARADzYcgRRHfEb7blL9EXSqtSC9qbxEdmzeCdZUdX0CcXG";
+
+    public static final String TOKEN = "1362202940241543170-70hzLjsBZkcx1ntpAkU2p87xNbpIR9";
+
+    public static final String SECRET = "F0tNM3A2YzivHosrLXRfba5b7E6v0s28Be6PpCtfTT7yP";
+}
